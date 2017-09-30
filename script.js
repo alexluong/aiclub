2ac13452706af7cba89be3030b0fbaf374308463
ahtluong <aluong@crimson.ua.edu>
2017-09-30 16:27:45 -0500

    Routing

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 window.onload = function () {
+	const button = document.getElementById('trigger');
+	const dropdown = document.getElementById('dropdown');
 	const burger = document.querySelector('.navbar-burger');
 	const menu = document.querySelector('.navbar-menu');
+
+	button.addEventListener('click', () => {
+		console.log('click');
+  	dropdown.classList.toggle('is-active');
+	});
 
 	burger.addEventListener('click', () => {
 		console.log('click');
